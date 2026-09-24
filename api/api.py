bd9e9490da61fd0ad0005cbccc15625ed5f32fe7
mmd <mohammadshabahari@gmail.com>
2026-09-24 19:20:01 +0330

    feat(api): implement cars endpoint

--- api/api.py
+++ api/api.py
@@ -1,12 +1,41 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
+import mysql.connector
 
 app = FastAPI()
+
+pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="autoradar_pool",
+    pool_size=5,
+    host="127.0.0.1",
+    user="root",
+    password="m13910110sh",
+    database="autocar",
+)
 
 
 @app.get("/cars")
 async def get_cars(page: int, limit: int):
-    pass
+    conn = pool.get_connection()
+    query = """SELECT cars.code,cars.brand,cars.brand_fa,cars.title,cars.year,
+    cars.mileage,cars.location,prices.price,prices.type,prices.payment,
+    prices.prepayment,dealers.address,dealers.name,dealers.score,dealers.type
+FROM cars
+JOIN prices ON cars.code = prices.car_code
+LEFT jOIN dealers ON cars.dealer_id = dealers.id
+ORDER BY modified_date DESC
+LIMIT %s OFFSET %s;
+"""
+
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute(query, (limit, ((page - 1) * limit)))
+    cars = cursor.fetchall()
+    for car in cars:
+        if car["score"] is not None:
+            car["score"] = float(car["score"])
+    conn.close()
+    return JSONResponse(cars, status_code=status.HTTP_200_OK)
 
 
 @app.get("/cars/{car_code}")
